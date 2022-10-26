b2eec0d2816581c12ceab18c9fc80812f0f1a091
root <dsilverman47@gmail.com>
2022-10-26 20:43:47 +0000

    still wont work

--- server/server.js
+++ server/server.js
@@ -6,24 +6,21 @@ app.get("/", (req, res) => {
     res.sendFile("/etc/nginx/project/ui/ui.html");
 });
 
-app.get("/ui.js", (req, res) => {
-    res.sendFile("/etc/nginx/project/ui/ui.js")
-});
-
 app.get("/library", (req, res) => {
     res.header("X-CSE356", "6306d53f58d8bb3ef7f6be55");
     res.sendFile("/etc/nginx/project/example-crdt-main/dist/crdt.js");
 });
 
 app.get("/api/connect/:id", (req, res) => {
+    console.log(req.params.id);
     res.set({
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive'
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive'
     });
     res.flushHeaders();
 
-    res.write('');
+    res.write('retry: 10000\n\n');
 });
 
 app.post("api/op/:id", (req, res) => {
