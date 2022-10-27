bd71aa2145653517de4cc44a6db323e18705d255
root <dsilverman47@gmail.com>
2022-10-27 05:16:46 +0000

    backend stuff

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.listen(3000);
 
+let docList = {};
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.sendFile("/etc/nginx/project/ui/ui.html");
 });
@@ -12,7 +16,7 @@ app.get("/library", (req, res) => {
 });
 
 app.get("/api/connect/:id", (req, res) => {
-    console.log(req.params.id);
+    //console.log(req.params.id);
     res.writeHead(200, {
       'Cache-Control': 'no-cache',
       'Content-Type': 'text/event-stream',
@@ -20,9 +24,30 @@ app.get("/api/connect/:id", (req, res) => {
     });
 
     res.write("event: sync\n");
-    res.write("data: test\n\n");
+    let docContents;
+    if (docList[req.params.id]) {
+        docContents = docList[req.params.id].contents;
+    }
+    else {
+        docList[req.params.id] = { "contents": "", "resObjs": [] };
+        docContents = "";
+    }
+    docList[req.params.id].resObjs.push(res);
+    res.write("data: " + docContents + "\n\n");
+    
+    res.on("close", function() {
+        docList[req.params.id].resObjs = docList[req.params.id].resObjs.filter(item => item != res);
+    });
 });
 
-app.post("api/op/:id", (req, res) => {
-    res.json({"status": "OK"});
+app.post("/api/op/:id", (req, res) => {
+    let payload = req.body;
+    let update = [{"insert": "hello"}];
+    if (docList[req.params.id]) {
+        for (let i = 0; i < docList[req.params.id].resObjs.length; i++) {
+            let resObj = docList[req.params.id].resObjs[i];
+            resObj.write("event: update\n");
+            resObj.write("data: " + JSON.stringify(update) + "\n\n");
+        }
+    }
 });
